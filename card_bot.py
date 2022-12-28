89521b4bee7c442deca051e7b0e150e0e3fe5d41
Alex <119233179+AlexSadovnikov-qa@users.noreply.github.com>
2022-12-28 21:06:28 +0300

    Update card_bot.py

--- card_bot.py
+++ card_bot.py
@@ -9,7 +9,7 @@ from telebot import TeleBot, types
 from faker import Faker
 
 
-bot = TeleBot(token='5697413879:AAE-vX2C-9wPgnrO-6MvMYCKLWDt9E2VjIc', parse_mode='html') # создание бота
+bot = TeleBot(token='Вставьте_свой_токен', parse_mode='html') # создание бота
 
 faker = Faker() # утилита для генерации номеров кредитных карт
 
